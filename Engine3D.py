b9b8962622c148ea92ba947df4baf7f744b2461a
Jerry Yu <jerryyu8@gmail.com>
2016-11-24 16:20:42 -0500

    Add edges

--- Engine3D.py
+++ Engine3D.py
@@ -20,17 +20,17 @@ class Point(object):
         self.surface = surface
         self.screenHeight = surface.get_height()
 
-        # have camera the so (0,0) is the middle
-        self.camX = int(surface.get_width() / 2)
-        self.camY = int(surface.get_height() / 2)
+        # have (0,0) point be in the middle
+        self.cX = int(surface.get_width() / 2)
+        self.cY = int(surface.get_height() / 2)
 
         # Adding Distortion for z coordinate to be drawn
         # Larger z towards center
         # z best works from range 200 to 300
         self.drawX = int(self.x * ((self.screenHeight / 2) / self.z) +
-                    self.camX)
+                    self.cX)
         self.drawY = int(self.y * ((self.screenHeight / 2) / self.z) +
-                    self.camY)
+                    self.cY)
 
 
     def drawPoint(self):
@@ -48,8 +48,9 @@ class Cube(object):
         centerY = pos[1]
         centerZ = pos[2]
         side = length/2
-        #z dimension is half of the x and y dimensions
+        # z dimension is half of the x and y dimensions
         zSide = length/4
+        # points of the cube
         self.points = [
           Point(centerX - side, centerY - side, centerZ - zSide + minZ, surface),
           Point(centerX + side, centerY - side, centerZ - zSide + minZ, surface),
@@ -60,11 +61,32 @@ class Cube(object):
           Point(centerX + side, centerY + side, centerZ + zSide + minZ, surface),
           Point(centerX - side, centerY + side, centerZ + zSide + minZ, surface)
           ]
+        # edges of the cube (point1, point2)
+        self.edges = [
+                (0,1),(1,2),(2,3),(3,0),
+                (4,5),(5,6),(6,7),(7,4),
+                (0,4),(1,5),(2,6),(3,7)
+                ]
 
 
     def draw(self, surface):
+        #draws points
         for point in self.points:
             point.drawPoint()
+        #draws edges
+        for edge in self.edges:
+            point1Index, point2Index = edge[0], edge[1]
+            point1 = self.points[point1Index]
+            point2 = self.points[point2Index]
+
+            pygame.draw.line(
+                surface,
+                (255,0,0),
+                (point1.drawX, point1.drawY),
+                (point2.drawX, point2.drawY),
+                20
+                )
+
 
 
 
